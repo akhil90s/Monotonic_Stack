41c3da7b7fd4a65ee582968dc45c9e23efadacea
akhil90s <akhil.chawla2010@gmail.com>
2023-01-04 22:00:13 +0530

    Monotonic_Stack

--- src/com/stack/Next_Greater_Element_On_Left.java
+++ src/com/stack/Next_Greater_Element_On_Left.java
@@ -2,26 +2,26 @@ package com.stack;
 
 import java.util.Stack;
 
-public class Next_Greater_Element_On_Right {
+public class Next_Greater_Element_On_Left {
 
 	public static void main(String[] args) {
 
 		int[] nums = new int[] { 2, 5, 9, 3, 1, 12, 6, 8, 7 };
-		int[] result = nextGreaterElementOnRight(nums);
+		int[] result = nextGreaterElementOnLeft(nums);
 		System.out.println(result);
 
 	}
 
 	// For greater problems, use a monotonically increasing stack (from top to bottom). 
 	// If we need to pop smaller elements from the stack before pushing a new element.
-	// For right we iterate with end of the array.
-	public static int[] nextGreaterElementOnRight(int[] nums) {
+	// For left we iterate with start of the array.
+	public static int[] nextGreaterElementOnLeft(int[] nums) {
 		int[] nge = new int[nums.length];
 		Stack<Integer> stack = new Stack<>();
-		nge[nums.length - 1] = -1;
-		stack.push(nums[nums.length - 1]);
-		for (int i = nums.length - 2; i >= 0; i--) {
-			while (!stack.isEmpty() && nums[i] >= stack.peek()) {
+		nge[0] = -1;
+		stack.push(nums[0]);
+		for (int i = 1; i < nums.length; i++) {
+			while (stack.isEmpty() && nums[i] >= stack.peek()) {
 				stack.pop();
 			}
 			if (stack.isEmpty()) {
@@ -29,7 +29,7 @@ public class Next_Greater_Element_On_Right {
 			} else {
 				nge[i] = stack.peek();
 			}
-			stack.push(nums[i]);
+			stack.push(nge[i]);
 		}
 		return nge;
 	}
